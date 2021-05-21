047716e8de13ba92a188236b86c7a0d8425d7c92
Prateek Bansal <prateekbansal64@gmail.com>
2021-05-21 23:19:39 +0530

    finished the project

---
+++ config.js
@@ -0,0 +1,10 @@
+const TWO_HOURS = 1000*60*60*24;
+module.exports = {
+    MONGODB: 'mongodb+srv://prateek-new:itsprateekbansal@cluster0.g5cto.mongodb.net/merng?retryWrites=true&w=majority',
+    PORT : 3000,
+    SESS_LIFETIME : TWO_HOURS,
+    SESS_NAME : 'sid',
+    SESS_SECRET : 'iamprateek,hello',
+    RAZOR_PAY_KEY_ID: 'rzp_test_tnPHGLOSuXuFNE',
+    RAZOR_PAY_KEY_SECRET: 'tvIv3jspaAK7ZCVPQ19U12vJ',
+}
